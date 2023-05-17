76d1584ed6e6af61676b10ef32ca5adbb36876e2
Pakapakk <132637313+Pakapakk@users.noreply.github.com>
2023-05-17 08:43:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+//1. Creating make file
+//2. Swap the value in the array using pointers** to help
+//3. Take input from argv
+
+
+int main(int argc, char **argv){
+ int N, *a;
+			N=argc-1;
+ 		     a=new int[N];
+			for(int i=0;i<N;i++) {
+			    a[i]=atoi(argv[i]);
+		         cout<< "a[" << i << "]=" <<a[i]<<endl;
+		}
+
+//void main(){
+    
+	   p=new int[n];
+
+ for(i=0;i<n;i++) {
+ 		   cin>>*p;//p is the location where p points to
+      p++;  
+ }
+ p-=n; //rewind the p to the starting location
+     // Using p within your program 
+  for(i=0;i<n;i++) {
+ 		   cout<<p[i]<<" "; //p is the location where p points to
+ //     p++;  
+ }
+
+ delete [] p;  
+}
+
+  
+//}
+
